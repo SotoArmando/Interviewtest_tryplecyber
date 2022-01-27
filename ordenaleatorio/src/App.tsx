50b5c06a589f45c3304ee4cbdced1c954a79a939
sososim <armando29@live.com>
2022-01-27 16:22:03 -0500

    Added go button to generate more numbers

--- ordenaleatorio/src/App.tsx
+++ ordenaleatorio/src/App.tsx
@@ -6,17 +6,20 @@ import Randomnnumbers from './Brain';
 function App() {
   const [numberElements, setNumberElements] = useState<number>(10);
   const [exceptionNumberisnotreal] = [(e:number) => (e < 0 ? 'Number is not real' : '')] 
-
+  const [randomOutput, setOuput] = useState<Array<number>>(Randomnnumbers(numberElements));
   const handleSequencyUpdate = function (e: React.FormEvent<HTMLInputElement>): void {
     setNumberElements(parseInt(e.currentTarget.value));
   }
-
+  const regenerate = function(): void {
+    setOuput(Randomnnumbers(numberElements))
+  }
   return (
     <div className="App" >
     <input value={numberElements} type="number" onChange={handleSequencyUpdate} />
+    <button onClick={regenerate}>Go</button>
     <div>Output: </div>
       {
-        [Randomnnumbers(numberElements).map(e =>
+        [randomOutput.map(e =>
           <div>{e}</div>
         ),
         <span className="error">{exceptionNumberisnotreal(numberElements)}</span>
