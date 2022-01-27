09562b4460b46964ffb28566abdaf6635e0a6e4b
sososim <armando29@live.com>
2022-01-27 16:28:44 -0500

    Number are meant to be different and higher than 0

--- ordenaleatorio/src/Brain.tsx
+++ ordenaleatorio/src/Brain.tsx
@@ -2,7 +2,11 @@ export default function Randomnnumbers(n: number): number[] {
     let result: number[] = [];
  
     while (result.length < n) {
-        result.push(Math.round(Math.random() * n))
+        let num: number = Math.round(Math.random() * n);
+        while (num == 0 || result.includes(num)) {
+            num = Math.round(Math.random() * n);
+        }
+        result.push(num);
     }
     return result;
 }
